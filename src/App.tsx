ada01745e2eb6e8ff7eb582d6d6d7b9e19d5f97a
Kazachenko Vlad <kazachenko2v@gmail.com>
2023-09-11 15:48:53 +0300

    add router basename

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { MainPage, ErrorPage, BookPage } from "./pages";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/:bookId",
-    element: <BookPage />,
-    errorElement: <ErrorPage />,
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <MainPage />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/:bookId",
+      element: <BookPage />,
+      errorElement: <ErrorPage />,
+    },
+  ],
+  { basename: "/librarry" }
+);
 
 function App() {
   return (
